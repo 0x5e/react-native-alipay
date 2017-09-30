f86d03b2d0b5ba72e2e3c7b556d107ca7868ea3d
gaosen <0x5e@sina.cn>
2017-09-30 21:38:18 +0100

    Implement iOS RCTAlipay

---
+++ ios/RCTAlipay.h
@@ -0,0 +1,20 @@
+//
+//  RCTAlipay.h
+//  RCTAlipay
+//
+//  Created by 高森 on 2017/9/30.
+//  Copyright © 2017年 0x5e. All rights reserved.
+//
+
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif
+
+#import <UIKit/UIKit.h>
+#import <AlipaySDK/AlipaySDK.h>
+
+@interface RCTAlipay : NSObject <RCTBridgeModule>
+
+@end
---
+++ ios/RCTAlipay.m
@@ -0,0 +1,50 @@
+//
+//  RCTAlipay.m
+//  RCTAlipay
+//
+//  Created by 高森 on 2017/9/30.
+//  Copyright © 2017年 0x5e. All rights reserved.
+//
+
+#import "RCTAlipay.h"
+
+@implementation RCTAlipay
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(pay:(NSString *)orderInfo
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    [AlipaySDK.defaultService payOrder:orderInfo fromScheme:self.appScheme callback:^(NSDictionary *resultDic) {
+        resolve(@[resultDic]);
+    }];
+}
+
+RCT_EXPORT_METHOD(payInterceptorWithUrl:(NSString *)urlStr
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    [AlipaySDK.defaultService payInterceptorWithUrl:urlStr fromScheme:self.appScheme callback:^(NSDictionary *resultDic) {
+        resolve(@[resultDic]);
+    }];
+}
+
+RCT_EXPORT_METHOD(getVersion:(RCTPromiseResolveBlock)resolve) {
+    resolve(@[AlipaySDK.defaultService.currentVersion]);
+}
+
+- (NSString *)appScheme {
+    NSArray *urlTypes = NSBundle.mainBundle.infoDictionary[@"CFBundleURLTypes"];
+    for (NSDictionary *urlType in urlTypes) {
+        if ([@"alipay" isEqualToString:urlType[@"CFBundleURLName"]]) {
+            NSArray *schemes = urlType[@"CFBundleURLSchemes"];
+            return schemes.firstObject;
+        }
+    }
+    return nil;
+}
+
+@end
