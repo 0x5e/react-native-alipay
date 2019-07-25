c7b1ead13e739b19a1fccd8c96ef3942c43a745c
史彬洪 <1114564087@qq.com>
2019-07-25 15:29:23 +0800

    updata

--- index.d.ts
+++ index.d.ts
@@ -1,31 +1,23 @@
-declare type TStatus = '9000' | '8000' | '4000' | '5000' | '6001' | '6002';
+type TStatus = '9000' | '8000' | '4000' | '5000' | '6001' | '6002';
 
 interface IResult {
-	resultStatus: TStatus;
-	result: string;
-	memo: string;
+  resultStatus: TStatus;
+  result: string;
+  memo: string;
 }
 
-interface IObject {
-	[key: string]: number | string;
+declare namespace Alipay {
+  
+  function authWithInfo(infoStr: string): IResult;
+
+  function pay(infoStr: string): IResult;
+
+  function payInterceptorWithUrl(infoStr: string): {
+    resultCode: TStatus;
+    returnUrl: string;
+  };
+  
+  function sign(infoStr: string): string;
 }
 
-declare function authWithInfo(infoStr: string): IResult;
-
-declare function pay(orderStr: string): IResult;
-
-declare function payInterceptorWithUrl(h5PayUrl: string): {
-	resultCode: TStatus;
-	returnUrl: string;
-};
-
-declare function sign(object: IObject, privateKey: string): string;
-
-
-declare const _default: {
-	authWithInfo: typeof authWithInfo;
-	pay: typeof pay;
-	payInterceptorWithUrl: typeof payInterceptorWithUrl;
-	sign: typeof sign;
-};
-export default _default;
+export = Alipay;
